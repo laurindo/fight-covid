3dfd16326c55bf0db64d320c2fdb2f165a0ecc5e
Daniel <dslaurindo@gmail.com>
2020-07-12 10:15:34 -0300

    Updates

---
+++ precache-manifest.0288ecf30c81d4df9596631fab008114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85f753f0e998f06976653af00e64421",
+    "url": "/fight-covid/index.html"
+  },
+  {
+    "revision": "327f17c714ae321fefc6",
+    "url": "/fight-covid/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "43a76e0800bc55f18735",
+    "url": "/fight-covid/static/js/2.6103e10c.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/fight-covid/static/js/2.6103e10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327f17c714ae321fefc6",
+    "url": "/fight-covid/static/js/main.6a228479.chunk.js"
+  },
+  {
+    "revision": "4ea34aa58849679840a1",
+    "url": "/fight-covid/static/js/runtime-main.466d365d.js"
+  }
+]);
